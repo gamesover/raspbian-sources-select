cc44ce54532390ab515aae696fee4b1ad21b5497
Ken <ken.ding@mail.com>
2018-04-09 02:30:35 +0930

    change to api mode

--- config/application.rb
+++ config/application.rb
@@ -13,5 +13,7 @@ module RaspbianSourcesSelect
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.api_only = true
   end
 end
